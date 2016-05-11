4ff7c896ac661061da88191ac781b28bcd505f98
LookRain <brantscube@hotmail.com>
2016-05-11 17:24:33 +0800

    ported original website from AWS, changed countdown, added popup form for early signup

---
+++ js/popupform.js
@@ -0,0 +1,14 @@
+// Plugin options and our code
+$("#modal_trigger").leanModal({
+		top: 100,
+		overlay: 0.6,
+		closeButton: ".modal_close"
+});
+
+$(function() {
+		// Calling Login Form
+	
+	$(".user_register").show();
+	return false;
+		
+});
